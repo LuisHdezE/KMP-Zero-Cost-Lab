44728b97289a123582fc2251d26a8c37f51163ad
MSc Luis A. Hernández Elias <88979457+LuisHdezE@users.noreply.github.com>
2026-09-17 22:54:50 -0300

    fix(android-test): use interaction member assertions

--- androidApp/src/androidTest/kotlin/dev/eliasworks/kmpzerocostlab/android/ProductCrudUiTest.kt
+++ androidApp/src/androidTest/kotlin/dev/eliasworks/kmpzerocostlab/android/ProductCrudUiTest.kt
@@ -1,9 +1,7 @@
 package dev.eliasworks.kmpzerocostlab.android
 
-import androidx.compose.ui.test.assertDoesNotExist
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextContains
-import androidx.compose.ui.test.fetchSemanticsNodes
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithTag
